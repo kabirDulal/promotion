79490d515b47fb9e99720cf7eaf7928552ed84f9
Kabir Dulal <kabird@stridesolutions.com.au>
2025-03-17 13:33:17 +1100

    added the svg package

--- lib/betr_promotion.dart
+++ lib/betr_promotion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class BetrPromotion extends StatelessWidget {
   const BetrPromotion({super.key});
@@ -6,7 +7,17 @@ class BetrPromotion extends StatelessWidget {
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [],
+        backgroundColor: const Color.fromARGB(1, 9, 58, 211),
+        actions: [
+          const Padding(padding: EdgeInsets.all(12)),
+          Row(
+            children: [
+              SvgPicture.asset('assets/images/icons/Group4315.svg'),
+              //const Spacer(),
+              SvgPicture.asset('assets/images/icons/BetRIcons.svg'),
+            ],
+          )
+        ],
       ),
       body: const Text('new app'),
     );
