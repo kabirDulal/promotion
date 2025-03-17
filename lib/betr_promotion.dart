62b75a82ecdc57d073573da2692ea19fc88cc3e1
Kabir Dulal <kabird@stridesolutions.com.au>
2025-03-17 12:09:49 +1100

    created a repository and added files

---
+++ lib/betr_promotion.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BetrPromotion extends StatelessWidget {
+  const BetrPromotion({super.key});
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+      ),
+      body: const Text('new app'),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:promotion/betr_promotion.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: BetrPromotion(),
+    ),
+  );
+}
