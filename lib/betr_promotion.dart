c2c5251e5961606d29a36ccc205e84f1eea32e49
Kabir Dulal <kabird@stridesolutions.com.au>
2025-03-17 19:03:07 +1100

    promotion and diamond shaped executed using transform.rotate

--- lib/betr_promotion.dart
+++ lib/betr_promotion.dart
@@ -11,6 +11,8 @@ class BetrPromotion extends StatelessWidget {
           padding: const EdgeInsets.only(bottom: 22, top: 12),
           child: SvgPicture.asset(
             'assets/images/icons/Group4315.svg',
+            colorFilter: const ColorFilter.mode(
+                Color.fromRGBO(9, 58, 211, 1), BlendMode.dst),
             height: 50.2,
             width: 351,
           ),
@@ -24,7 +26,77 @@ class BetrPromotion extends StatelessWidget {
           ),
         ],
       ),
-      body: const Text('new app'),
+      body: Container(
+        margin: const EdgeInsets.only(
+          top: 18,
+          left: 12,
+          right: 12,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SvgPicture.asset(
+                  'assets/images/icons/promoIcon.svg',
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                const Text(
+                  'Exclusive Promotions',
+                  style: TextStyle(
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    height: 1.50,
+                    color: Color.fromRGBO(35, 38, 43, 1),
+                  ),
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Transform.rotate(
+                      angle: 45 * 3.1415926535 / 180,
+                      child: Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: const Color.fromRGBO(255, 177, 7, 1),
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      '2',
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 13,
+                        height: 1,
+                        color: Color.fromRGBO(35, 38, 43, 1),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Container(
+              width: 351,
+              height: 160,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: const Color.fromRGBO(9, 58, 211, 1),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
