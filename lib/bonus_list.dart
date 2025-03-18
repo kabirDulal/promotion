b73119edeaceae7054354f0752d530ed6e26dd21
Kabir Dulal <kabird@stridesolutions.com.au>
2025-03-18 14:16:34 +1100

    updated the container

--- lib/bonus_list.dart
+++ lib/bonus_list.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:promotion/model/bonus.dart';
+import 'package:promotion/betr_promotion.dart';
 
 class BonusList extends StatelessWidget {
   const BonusList({required this.bonuses, super.key});
   final List<Bonus> bonuses;
   @override
   Widget build(context) {
-    return const Stack(
-      children: [],
+    var amount = 40.02;
+    var arr = amount.toStringAsFixed(2).split('.');
+    return Container(
+      height: 96,
+      width: 82,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(220, 232, 245, 1),
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          width: 2,
+          color: const Color.fromRGBO(9, 58, 211, 1),
+        ),
+      ),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 18,
+          ),
+          RichText(
+            text: const TextSpan(
+              children: [
+                TextSpan(
+                  text: "\$",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Color.fromRGBO(9, 58, 211, 1),
+                  ),
+                ),
+                TextSpan(text: ''),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
